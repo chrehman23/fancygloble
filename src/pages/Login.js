90634387d11b4413c91d8b36dceb0d91dd181ba4
Abdur Rehman <abdur.rehman@ivylabtech.com>
2021-09-27 19:27:00 +0500

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -215,7 +215,7 @@ class Login extends Component {
                                         <GoogleLogin
                                             socialId={process.env.REACT_APP_GOOGLE_LOGIN_KEY}
                                             className="google-login"
-                                            redirectUri={"http://localhost:3000/login"}
+                                            redirectUri={"https://master.d3gqmduw2r7u7n.amplifyapp.com/login"}
                                             fetchBasicProfile={true}
                                             responseHandler={this.responseGoogle}
                                             className='w-100 bg-transparent border-0'
